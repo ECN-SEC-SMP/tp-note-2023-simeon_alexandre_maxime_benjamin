657d3f8c58bfe19b8f9a5ebf58ea2bf896878d79
Simeonitoo <124591553+Simeonitoo@users.noreply.github.com>
2023-06-01 15:27:53 +0200

    ajout angle dans la classe case

--- Case/Case.cpp
+++ Case/Case.cpp
@@ -1,12 +1,14 @@
 #include "Case.hpp"
 
-Case::Case() : murH(false), murD(false), murB(false), murG(false), robot(nullptr), target(nullptr) {}
+Case::Case() : murH(false), murD(false), murB(false), murG(false), angle(NONE), robot(nullptr), target(nullptr) {}
 
 // Getteurs
 bool Case::getMurH() const{ return murH; }
 bool Case::getMurD() const{ return murD; }
 bool Case::getMurB() const{ return murB; }
 bool Case::getMurG() const{ return murG; }
+
+Angle Case::getAngle() const{ return angle; }
 
 Robot* Case::getRobot() const{ return robot; }
 Target* Case::getTarget() const{ return target; }
@@ -16,6 +18,8 @@ void Case::setMurH(bool mur){ murH = mur; }
 void Case::setMurD(bool mur){ murD = mur; }
 void Case::setMurB(bool mur){ murB = mur; }
 void Case::setMurG(bool mur){ murG = mur; }
+
+void Case::setAngle(Angle newAngle){ angle = newAngle; }
 
 void Case::setRobot(Robot* newRobot) { robot=newRobot; }
 void Case::setTarget(Target* newTarget) { target = newTarget; }
--- Case/Case.hpp
+++ Case/Case.hpp
@@ -5,9 +5,18 @@
 #include "../Robot/Robot.hpp"
 #include "../Target/Target.hpp"
 
+enum Angle {
+    HD,
+    BD,
+    BG,
+    HG,
+    NONE
+};
+
 class Case {
 private:
     bool murH, murD, murB, murG;
+    Angle angle;
     Robot* robot;
     Target* target;
 
@@ -20,11 +29,15 @@ public:
     void setMurB(bool mur);
     void setMurG(bool mur);
 
+    void setAngle(Angle newAngle);
+
     // Getteurs
     bool getMurH(void) const;
     bool getMurD(void) const;
     bool getMurB(void) const;
     bool getMurG(void) const;
+
+    Angle getAngle(void) const;
 
     // Robot
     void setRobot(Robot* newRobot);
