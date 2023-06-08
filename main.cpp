77db30ce9347c2415c1bcb9029bea499ddc557b4
alexandre dujardin <alexandre.dujardin1@outlook.com>
2023-06-08 12:01:44 +0200

    affichage angle aleatoire

--- main.cpp
+++ main.cpp
@@ -3,27 +3,65 @@
 #include "Position/Position.hpp"
 #include "Robot/Robot.hpp"
 #include "Target/Target.hpp"
+#include "Case/Case.hpp"
+#include "utilitaires/utilitaires.hpp"
 
 using namespace std;
 
 int main() { 
-    cout << "Hello everybody!\n"; 
 
-    // Instanciation
-    // Position pos1(10, 5);
-    // Robot robot1(BLUE, pos1);
-    // Target t(RED, CIRCLE);
-    // Game g( Robot& robots, Target& targets);
-    // g.iniGame(); 
-    // cout << "Couleur du robot : " << robot1.getColor() << endl;
-    std::vector<Robot> robots; // Create a vector of Robot objects
-    std::vector<Target> targets; // Create a vector of Target objects
+    Case* plateau[16][16] = {nullptr};
 
-    // Add Robot and Target objects to the vectors (example)
-    // robots.push_back(Robot(/* arguments for Robot constructor */));
-    // targets.push_back(Target(/* arguments for Target constructor */));
+    // Robot* r_red;
+    // Robot* r_blue;
+    // Robot* r_yellow;
+    // Robot* r_green;
 
-    Game g(robots, targets); // Instantiate the Game object with the vectors of robots and targets
-    g.iniGame();
+    // Target* target;
+
+    creerPlateau(plateau);
+
+    //target
+    //target = new Target(YELLOW,DIAMOND,Position(4,4));
+
+    //mise en place robot
+
+    // r_red = new Robot(RED,Position(9,6));
+    // r_blue = new Robot(BLUE,Position(9,7));
+    // r_yellow = new Robot(YELLOW,Position(9,8));
+    // r_green = new Robot(GREEN,Position(9,9));    
+    
+    // plateau[r_red->getPosition().getX()][r_red->getPosition().getY()]->setRobot(r_red);
+    // plateau[r_blue->getPosition().getX()][r_blue->getPosition().getY()]->setRobot(r_blue);
+    // plateau[r_yellow->getPosition().getX()][r_yellow->getPosition().getY()]->setRobot(r_yellow);
+    // plateau[r_green->getPosition().getX()][r_green->getPosition().getY()]->setRobot(r_green);
+
+    //mi enplace angle
+    for(int i = 0;i<4;i++)
+    {
+        for(int i = 0;i < 100000000;i++){}
+        angleAleatoire(plateau,premier_quart);
+        for(int i = 0;i < 100000000;i++){}
+        angleAleatoire(plateau,deuxieme_quart);
+        for(int i = 0;i < 100000000;i++){}
+        angleAleatoire(plateau,troisieme_quart);
+        for(int i = 0;i < 100000000;i++){}
+        angleAleatoire(plateau,quatrieme_quart);
+        for(int i = 0;i < 100000000;i++){}
+    }
+
+    
+
+
+
+
+    //mise en place mur aleatoire
+    // mur_aleatoire(plateau);
+    // angleAleatoire(plateau,premier_quart);
+     
+    // plateau[4][4]->setAngle();
+    // plateau[4][4]->setTarget(target);
+    //afficher plateau
+    afficher_plateau(plateau);
      
 }
